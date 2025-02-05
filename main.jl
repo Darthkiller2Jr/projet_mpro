c75d34da78dc338f7f4091a1e9be79f18135dba6
Dimitri <d.desaintguilhem@gmail.com>
2025-02-05 15:25:00 +0100

    chgmt main

--- main.jl
+++ main.jl
@@ -2,8 +2,9 @@ include("dual.jl")
 include("plans_coupants.jl")
 
 function main(time_limit)
-    println("Dualisation")
-    main_dual(time_limit)
+
+    println("Algorithme de branch and cut avec heuristique")
+    main_branch_and_cut(time_limit, true)
 
     println("Algorithme de plans coupants")
     main_plans_coupants(time_limit)
@@ -11,6 +12,6 @@ function main(time_limit)
     println("Algorithme de branch and cut sans heuristique")
     main_branch_and_cut(time_limit, false)
 
-    println("Algorithme de branch and cut avec heuristique")
-    main_branch_and_cut(time_limit, true)
+    println("Dualisation")
+    main_dual(time_limit)
 end
